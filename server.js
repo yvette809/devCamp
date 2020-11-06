a2db59aca451c8187aaf77c4f01444d708d28e8a
Yvette <nchombuayvta@gmil.com>
2020-11-06 22:14:36 +0100

    json files

--- server.js
+++ server.js
@@ -2,10 +2,21 @@ const express = require ("express")
 const server = express()
 const cors = require ("cors")
 const dotenv = require("dotenv")
+const mongoose = require ("mongoose")
 
 dotenv.config()
 
 const port = process.env.PORT || 4020
-server.listen(port, ()=>{
-    console.log(`server running on port ${port}`)
-})
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(
+    server.listen(port, () => {
+      console.log(`server running on port ${port}`);
+    })
+  )
+  .catch((error) => console.log(error));
